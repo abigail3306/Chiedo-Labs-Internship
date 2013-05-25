de90dd6c0b0e71bf7845044f4db0bda6a12ffaf0
Chiedo John <cj@chiedojohn.com>
2013-05-25 11:58:27 -0400

    Added dashboard cleanup

--- functions.php
+++ functions.php
@@ -157,4 +157,22 @@ function g_plupload_action() {
     echo $status['url'];
     exit;
 }
+
+/**
+ * Remove default wordpress dashboard wigets except for right now
+ */
+add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
+function remove_dashboard_widgets() {
+  global $wp_meta_boxes;
+
+  unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+  unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+  //unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+  unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
+  //unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
+  unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+  unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
+  unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+
+}
 ?>
