55130cd188640862afb286bc683081ec659ef14f
Chiedo John <cj@chiedojohn.com>
2013-05-25 13:46:44 -0400

    more

--- functions.php
+++ functions.php
@@ -159,20 +159,21 @@ function g_plupload_action() {
 }
 
 /**
- * Remove default wordpress dashboard wigets except for right now
+ * Remove default wordpress dashboard wigets except a few
  */
 add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
 function remove_dashboard_widgets() {
   global $wp_meta_boxes;
 
+  $wp_meta_boxes['dashboard']['normal']['core']['dashboard_primary'] = $wp_meta_boxes['dashboard']['side']['core']['dashboard_primary'];
+  
   unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
   unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-  //unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+  unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
   unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
-  //unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
   unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
   unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-  unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+  //unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 
 }
 
