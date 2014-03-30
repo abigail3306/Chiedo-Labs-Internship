71ca80befd2e4e7f6ae7485873b7713304f5a680
Chiedo John <cj@chiedojohn.com>
2014-03-30 16:24:17 -0400

    Minor

--- front-page.php
+++ front-page.php
@@ -4,17 +4,6 @@
   <!-- You can replace the following with home page content -->
   <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
   <?php the_content(); ?>
-  <!--READ ME. DO NOT DELETE UNTIL YOU IMPLEMENT.-->
-<h1>For security purposes, you need to create the following two files.</h1>
-  wp-content/uploads/.htaccess
-
-  The file will have the following contents in bold (obviously do not copy the bold tags).
-  <b>
-    #This should block execution of PHP in this directory. This is good to block hacker
-    <Files *.php*>
-        deny from all
-    </Files>
-  </b>
 </div>
 <?php endwhile;?>
 <?php get_footer(); ?>
